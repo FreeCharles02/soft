f27d1f4019f2cb62e489559d0bdbb94737a98c27
Charles <105826694+FreeCharles02@users.noreply.github.com>
2024-07-08 21:55:41 -0500

    n

---
+++ page.js
@@ -0,0 +1,12 @@
+import Dashboard from "./app/components/dashboard.jsx";
+import Sidebar from "./app/components/sidebar.jsx";
+import Topbar from "./app/components/topbar.jsx";
+export default function Home() {
+  return (
+ <div>
+<Topbar/>
+<Dashboard />
+<Sidebar />
+</div>
+  );
+}
